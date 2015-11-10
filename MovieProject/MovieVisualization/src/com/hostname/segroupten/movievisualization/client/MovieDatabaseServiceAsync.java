4839b899dd434e0fa7864be345b1a4993506deee
Jan von der Assen <jvdassen@dizmo.com>
2015-11-10 23:42:53 +0100

    deleted old files
    deleted files, that were out of sync. new (manually merged) source code
    will be commited soon

--- MovieProject/MovieVisualization/src/com/hostname/segroupten/movievisualization/client/MovieDatabaseServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package com.hostname.segroupten.movievisualization.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.hostname.segroupten.movievisualization.shared.Movie;;
-
-public interface MovieDatabaseServiceAsync {
-void getMovies( AsyncCallback<Movie[]> callback);
-}
--- MovieProject/MovieVisualization/src/com/hostname/segroupten/movievisualization/shared/Mov.java
+++
@@ -1,8 +0,0 @@
-package com.hostname.segroupten.movievisualization.shared;
-
-public class Mov {
-public  String name;
-
-
-public Mov(){};
-}
